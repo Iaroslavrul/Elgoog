2fd00bfde48cf5f3b4eb7745026aea3c91b29717
Yarik_air <iaroslavrul@gmail.com>
2020-11-25 01:45:23 +0200

    Fix tree scraping

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import uvicorn
-from fastapi import FastAPI, Query, Request
-from fastapi.responses import Response
+from fastapi import FastAPI, Query
 import requests
 import re
 import sqlite3
 import distance
 import collections
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+import urllib3
+urllib3.disable_warnings()
 
 DATABASE_LOCAL = sqlite3.connect('db.sqlite3', check_same_thread=False)
 sqlite3_cursor = DATABASE_LOCAL.cursor()
@@ -37,12 +36,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-templates = Jinja2Templates(directory="templates")
-
-
 
 def sql_insert(q, title_, data):
-    sqlite3_cursor.execute("""INSERT INTO scan (link, title, text) VALUES (?,?,?)""", (q, title_, data))
+    sqlite3_cursor.execute("""INSERT OR IGNORE INTO scan (link, title, text) VALUES (?,?,?)""", (q, title_, data))
     DATABASE_LOCAL.commit()
 
 
@@ -69,45 +65,79 @@ def search_by_words():
 # TODO Добавить поиск ссылок на внутренних ссылках
 
 
+def shorter_link(link):
+    print('input link: ', link)
+    if link[:8] == 'https://':
+        link = link[8:]
+    elif link[:7] == 'http://':
+        link = link[7:]
+    if link[:4] == 'www.':
+        link = link[4:]
+    if link[:4] == 'www.':
+        link = link[4:]
+    if link[-1] == '/':
+        link = link[:-1]
+    print('output link: ', link)
+    return link
+
+
+def get_data_from_response(link):
+    response = requests.get(link, timeout=30, verify=False)
+    title = re.search(r'<title>(.*?)</title>', response.content.decode('utf-8')).group(0)[7:-8]
+    data = re.sub(r'\<(.*?)\>|\n', '', response.content.decode('utf-8'))
+    data = re.sub(r'<script>(.*?)</script>', '', data)
+    data = re.sub(r'\s{2,}', ' ', data).replace('"', "'")
+    return title, data
+
+
 @app.get('/index/')
 def input_link(q: str = Query(None, min_length=4, description='Input in this query your link',
                               regex="http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")):
     if q:
-        response = requests.get(q, timeout=30, verify=False).content.decode('utf-8')
-        title = re.search(r'<title>(.*?)</title>', response).group(0)[7:-8]
-        data = re.sub(r'\<(.*?)\>|\n', '', response)
-        data = re.sub(r'<script>(.*?)</script>', '', data)
-        data = re.sub(r'\s{2,}', ' ', data).replace('"', "'")
-        if q[:8] == 'https://':
-            q = q[8:]
-        elif q[:7] == 'http://':
-            q = q[7:]
-        if q[:4] == 'www.':
-            q = q[4:]
-        if q[:4] == 'www.':
-            q = q[4:]
-        if q[-1] == '/':
-            q = q[:-1]
+        title, data = get_data_from_response(q)
+        q = shorter_link(q)
+        print("q: ", q)
         try:
             sql_insert(q, title, data)
-            return {"condition": "Ссылка добавлена"}
-        except Exception:
-            return {"condition": "Эта ссылка уже есть в базе"}
-        # внешние ссылки
-        # links = re.findall(r'(<a.*href=\")(htt.*?)(\")', response)
-        # outer_links=[]
-        # for i, link in enumerate(links):
-        #     try:
-        #         if len(requests.get(link[1], timeout=30, verify=False).history) == 0:
-        #            outer_links.append(link[1])
-        #     except Exception as e:
-        #         print(e)
-        # return {"input link": outer_links}
+            depth = 3
+            url_list_depth = [[] for i in range(0, depth + 1)]
+            url_list_depth[0].append(f'https://{q}')
+            for depth_i in range(0, depth):
+                print('depth_i ', depth_i)
+                for links in url_list_depth[depth_i]:
+                    print('links ', links)
+                    domain = re.search(r'^((http[s]?):\/)?\/?([^:\/\s]+)', links).group(3)
+                    response = requests.get(links, verify=False, timeout=30).content.decode('utf-8')
+                    outside_links = list(set(map(lambda link: link[1],
+                                                re.findall(r'(<a.*href=\")(htt.*?)(\")', response))))
+                    inside_links = list(set(map(lambda link: f'https://{domain}{link[1]}',
+                                                re.findall(r'(<a.*href=\")(/\w+.*?)(\")', response))))
+                    inside_links.extend(outside_links)
+                    for link in inside_links:
+                        print('!!! ', link)
+                        flag = False
+                        for item in url_list_depth:
+                            for l in item:
+                                if link == l:
+                                    flag = True
+                        if link is not None and flag is False and link not in url_list_depth[depth_i + 1] and len(
+                                requests.get(link, verify=False, timeout=30).history) == 0:
+                            url_list_depth[depth_i + 1].append(link)
+                            try:
+                                title, data = get_data_from_response(link)
+                                print('url_new: ', link)
+                                url_new = shorter_link(link)
+                                sql_insert(url_new, title, data)
+                            except:
+                                print('JS')
+            return url_list_depth
+        except Exception as e:
+            print(e)
     return "Start page"
 
 
-@app.get('/search/', response_class=HTMLResponse)
-def search(request: Request, q: str = Query('', min_length=4, description='Поиск')):
+@app.get('/search/')
+def search(q: str = Query('', min_length=4, description='Поиск')):
     data_rows = search_by_words()
     similarity = {}
     for data_row in data_rows:
@@ -115,9 +145,7 @@ def search(request: Request, q: str = Query('', min_length=4, description='По�
     od = collections.OrderedDict(sorted(similarity.items(), reverse=False))
     result_list = list(map(lambda x: od[x], list(od)))
     # Чем меньше - тем лучше
-    return templates.TemplateResponse("item.html", {"request": request, "result_list": result_list[:11]})
-    # return result_list[:11]
-
+    return result_list[:11]
 
 
 @app.get('/test/')
@@ -149,27 +177,3 @@ def test(q: str = Query(None, min_length=4, description='Input in this query you
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True, access_log=False)
-
-# @app.get('/test/')
-# def test(q: str = Query(None, min_length=4, description='Input in this query your link',
-#                               regex="http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")):
-#     depth = 5  # 3 levels
-#     count = 3  # amount of urls in each level
-#     url_list_depth = [[] for i in range(0, depth + 1)]
-#     url_list_depth[0].append(q)
-#     for depth_i in range(0, depth):
-#         for links in url_list_depth[depth_i]:
-#             response = requests.get(q).content.decode('utf-8')
-#             tags = re.findall(r'(<a.*href=\")(htt.*?)(\")', response)
-#             for link in tags:
-#                 url_new = link[1]
-#                 flag = False
-#                 for item in url_list_depth:
-#                     for l in item:
-#                         if url_new == l:
-#                             flag = True
-#
-#                 if url_new is not None and "http" in url_new and flag is False:
-#                     url_list_depth[depth_i + 1].append(url_new)
-#                     print(links, "->", url_new)
-#     return {"search": url_list_depth}
